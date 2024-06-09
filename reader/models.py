1e941667cba44cb71d7175e6c7aa551d1f7cc9a7
Entrix Makambi <makambientryx@gmail.com>
2024-06-09 20:25:07 +0300

    initial commit

--- reader/models.py
+++ reader/models.py
@@ -9,6 +9,7 @@ class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset()\
                     .filter(status=Post.Status.PUBLISHED)
+
             
 
 class Post(models.Model):
@@ -32,6 +33,7 @@ class Post(models.Model):
                             default=Status.DRAFT)
     objects=models.Manager()
     published=PublishedManager()
+    
 
     class Meta:
         ordering=['publish']
